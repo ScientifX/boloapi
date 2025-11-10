5936fc8d53cf8fafdee5912b9364ace29a7430a9
Jerome Bradenbaugh <jbradenbaugh@scientifics.io>
2025-11-10 08:38:00 -0800

    Daily db backup plus collateral changes on 2025-11-10

--- artifacts/boloapi-db-backup.ps1
+++ artifacts/boloapi-db-backup.ps1
@@ -1,4 +1,6 @@
-cd C:\Clients\SD\boloapi
+Set-Location C:\Clients\SD\boloapi
+
+Start-Transcript -Path "boloapi.log" -Append
 
 $env:PGPASSWORD = "Rxh1m3d3s!@#"
 & "C:\Program Files\PostgreSQL\18\bin\pg_dump.exe" -h localhost -p 5432 -U postgres -d postgres -F c -f "artifacts/boloapi_db_backup.dump"
@@ -8,3 +10,7 @@ Remove-Item Env:\PGPASSWORD
 & "C:\Program Files\Git\bin\git.exe" add .
 & "C:\Program Files\Git\bin\git.exe" commit -m "Daily db backup plus collateral changes on $(Get-Date -Format 'yyyy-MM-dd')"
 & "C:\Program Files\Git\bin\git.exe" push
+
+Stop-Transcript
+
+Start-Sleep -s 5
