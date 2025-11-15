31edc9e0c8fde3fb7b4ac7eac7f286b5e8bb53bb
Jerome Bradenbaugh <jbradenbaugh@scientifics.io>
2025-11-14 18:53:11 -0800

    Removing dbconfig.py

--- dbconfig.py
+++
@@ -1,8 +0,0 @@
-import os
-DB_CONFIG = {
-    "host": os.getenv('API_DB_HOST'),
-    "port": os.getenv('API_DB_PORT'),
-    "database": os.getenv('API_DB_DATABASE'),
-    "user": os.getenv('API_DB_USER'),
-    "password": os.getenv('API_DB_PASSWORD')
-    }
